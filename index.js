8b56980980e2a605b3c19b6f82959e4275a5b48b
BXGrzesiek <grzegorz.biskup2015@gmail.com>
2019-01-30 20:51:14 +0100

    routing
    label on screen

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 //set up express app
 const app = express();
 
+//wymuszenie użycia ścieżki /api => ../routes/api
+app.use('/api', routes('./routes/api'));
+
 //listen for request
-app.listen(process.env.port || 4000, function(){
+app.listen(process.env.port || 8082, function(){
   console.log('now listening for requests');
 });
